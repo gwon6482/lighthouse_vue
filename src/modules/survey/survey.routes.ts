2aa344e9b047dfa4b2fe05620a9edbd4ac12cfbd
winkslee <jakejhlee0210@gmail.com>
2026-01-27 15:21:41 +0900

    a

--- src/modules/survey/survey.routes.ts
+++ src/modules/survey/survey.routes.ts
@@ -1,17 +1,17 @@
 export default [
     {
       path: '/',
-      name: 'home',
+      name: 'Home',
       component: () => import('./pages/HomePage.vue'),
     },
     {
       path: '/self-understanding',
-      name: 'self-understanding',
+      name: 'Self Understanding',
       component: () => import('./pages/SelfUnderstandingPage.vue'),
     },
     {
       path: '/self-understanding/test',
-      name: 'survey-test',
+      name: 'Self Understanding Test',
       component: () => import('../../views/SurveyTestView.vue'),
     },
 ]
